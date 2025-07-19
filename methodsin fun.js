85a156556d3843bf92bdb4e374fd86da3d11fc5a
bodakaveri02 <bodakaveri02@gmail.com>
2025-07-19 09:57:16 +0530

    created methodsin fun.js
    added method properties.

---
+++ methodsin fun.js
@@ -0,0 +1,12 @@
+const cal={
+    add:function(a,b){
+        return a+b;
+    },
+    sub:function(a,b){
+        return a-b;
+    },
+    mul:function(a,b){
+        return a*b;
+    }
+}
+console.log(cal.add(2,6));
