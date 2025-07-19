6197921177bb7b924e2b1e34e3e71d3f2e5fb006
bodakaveri02 <bodakaveri02@gmail.com>
2025-07-19 10:01:00 +0530

    created  this.js
    added this properties to js

---
+++ this.js
@@ -0,0 +1,21 @@
+const student={
+    name:"kaveri",
+    age:20,
+    math:80,
+    eng:80,
+    sci:85,
+   /*getavg(){
+    let avg=(this.math+this.eng+this.sci)/3;
+        console.log(avg);
+    }
+};
+student.getavg();*/
+get(){
+ console.log(this);
+},
+getavg:()=>
+{
+    console.log(this);
+}
+}
+
